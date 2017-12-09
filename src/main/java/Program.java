a4891836ee03f1a3aa7492b4ac4534cc1390ba06
Тимур <chosti34@gmail.com>
2017-12-09 14:51:25 +0300

    Добавлен основной цикл приложения
    В конфигурации maven изменена версия компилятора

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,5 +1,32 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Program {
+    private static final String EXIT_COMMAND = "exit";
+    private static boolean running = true;
+    private static HashMap<String, Runnable> actionMap = new HashMap<String, Runnable>() {{
+        put("show", () -> System.out.println("showing something"));
+        put("exit", () -> running = false);
+    }};
+
+    private static void dispatchInput(final String input) {
+        Runnable command = actionMap.get(input);
+        if (command != null) {
+            command.run();
+        } else {
+            System.out.println("Unknown command!");
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println(MyMath.add(2, 3));
+        Scanner scanner = new Scanner(System.in);
+        while (running) {
+            System.out.print("> ");
+            if (scanner.hasNextLine()) {
+                dispatchInput(scanner.nextLine());
+            } else {
+                running = false;
+            }
+        }
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,11 @@
+public class Task {
+    private String text;
+
+    public void setText(final String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
